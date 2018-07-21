3e88c4e33d16abcbd24fa0680e934ab0f750c592
Brian Yeh <brian.yeh@bronci.com.tw>
2018-07-21 19:54:00 +0800

    Pass the test case of the cucumber.

--- cucumber/steps/CalculatorSteps.js
+++ cucumber/steps/CalculatorSteps.js
@@ -1,22 +1,25 @@
 import {Given, Before, When, Then} from 'cucumber';
+import Calculator from '../../src/Calculator';
+import assert from 'assert';
+
+firstInt = null;
+secondInt = null;
+calculator = null;
 
 Before({tags: "@Calculator"}, function() {
-  console.log("this is a Before block");
+  // console.log("this is a Before block");
+  calculator = new Calculator();
 });
 
 
 Given('I start with {int}', function (int) {
-  console.log("this is a given sentence");
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  firstInt = int;
 });
 
 When('I add {int}', function (int) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  secondInt = int;
 });
 
 Then('I end up with {int}', function (int) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  assert.equal(calculator.add(firstInt, secondInt), int);
 });  
