71084db26a4a4be48be31e6123c2222662684a75
Brian Yeh <brian.yeh@bronci.com.tw>
2018-07-21 16:43:11 +0800

    Add cucumber test cases

---
+++ cucumber/steps/CalculatorSteps.js
@@ -0,0 +1,23 @@
+const { Given, When, Then, Before } = require('cucumber')
+// import {Given} from 'cucumber';
+
+Before({tags: "@Calculator"}, function() {
+  console.log("this is a Before block");
+});
+
+
+Given('I start with {int}', function (int) {
+  console.log("this is a given sentence");
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('I add {int}', function (int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I end up with {int}', function (int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});  
